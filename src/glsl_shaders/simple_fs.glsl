7c118cb482276b9bc9050ea450dc12b0836940c3
david santiago <david.valente.santiago@gmail.com>
2024-01-21 08:43:33 -0300

    update

--- src/glsl_shaders/simple_fs.glsl
+++ src/glsl_shaders/simple_fs.glsl
@@ -1,6 +1,5 @@
 precision mediump float; // define a precisão de calculos de floating points
 uniform vec4 uPixelColor; // cor do pixel (uniform = constante p/ todos os vertices)
-
 void main(void){
     gl_FragColor = uPixelColor; // atribui para cada pixel da tela
 }
