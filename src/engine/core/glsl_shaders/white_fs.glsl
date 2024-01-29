ab95fcda36946fa23aa1110aa4460523e6ac7198
Almir <david.valente.santiago@gmail.com>
2024-01-29 16:14:54 -0300

    update

---
+++ src/engine/core/glsl_shaders/white_fs.glsl
@@ -0,0 +1,7 @@
+ // também conhecido como "pixelshader"
+// executada para cada pixel a ser desenhado na tela
+void main(void){
+    // gl_FragColor determina a cor do pixel
+    // neste caso, branco.
+    gl_FragColor = vec4(1,0.7,1,1.0);
+}
---
+++ src/engine/renderable.js
@@ -0,0 +1,24 @@
+import Engine from "./engine.js";
+import Transform from "./transform.js";
+
+class Renderable {
+    constructor() {
+        this.engine = new Engine();
+        this.gl = this.engine.getGl();
+        this.mShader = this.engine.getShaderResources().getConstColorShader();
+        this.mColor = [1, 1, 1, 1]; // color of pixel
+        this.mXform = new Transform();
+    }
+
+    draw() {
+        // configura os shaders com cor e dados de transformação
+        this.mShader.activate(this.mColor,this.mXform.getTRSMatrix()); // para ativar o shader
+        // desenhamos as polígonos com base nas configurações setadas no comando anterior
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+    }
+
+    setColor(color) {this.mColor = color; }
+    getColor() { return this.mColor; }
+    getXform() { return this.mXform;}
+}
+export default Renderable;
